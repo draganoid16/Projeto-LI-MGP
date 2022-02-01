15f91cbed3392d9c96a9cec6e5e7ec9d3c2606b7
draganoid16 <draganoid5@live.com>
2022-02-01 23:39:46 +0000

    Scroll fade in and out adicionado

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-//document.getElementById("cagado").style.display = "none";
-//document.getElementById("osga-moura").style.display = "none";
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
 
+
+
+window.addEventListener("scroll", reveal);
+//funcao para dar fade in e fade out da pagina
+
+//jogo.js
 var botao_anterior = document.getElementById("botao_anterior");
 var botao_reset = document.getElementById("botao_reset");
 var textodoanimal = document.getElementById("textofinal");
